d3164cab28ccb2a3e0e0ceaa62a54d82800d4a4f
LucasWinicki <85349671+LucasWinicki@users.noreply.github.com>
2021-10-06 09:04:08 -0300

    DescargaFotos

--- src/components/HomeUser.jsx
+++ src/components/HomeUser.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import axios from "axios";
 import db from "../firebase";
-import {Button, Navbar, Nav, NavDropdown, Form, FormControl, Container, Row, Col} from 'react-bootstrap';
+import {Button, Navbar, Nav, NavDropdown, Form, FormControl, Container, Row, Col, Image} from 'react-bootstrap';
 import logo from '../images/Logo.png'
 import "./Home.css";
 import Header from "./Header";
@@ -13,14 +13,14 @@ import { render } from "@testing-library/react";
 export default function HomeUser(props){
     const {handleLogout}=props;
     const [marcas, setMarcas] = useState([]);
-    const [marcaAVer, setMarcaAVer] = useState([]);
+    const [marcaAVer, setMarcaAVer] = useState("");
 
     useEffect(() => {
         async function GiveMeBrands() {
             await axios.get("http://localhost:8000/ListadoMarcas").then((res) => {
                 console.log(res.data);
                 setMarcas(res.data);
-                marcas.map((marca) => {
+                marcas.forEach((marca) => {
                     cargarLogos(marca);
                 })
             }).catch((error) => {
@@ -32,13 +32,12 @@ export default function HomeUser(props){
     
     function cargarLogos(marca){
         var storage = db.storage();
-            storage.child('images/'+marca.logo).getDownloadURL().then(function(url){
+            storage.ref('images/'+marca.logo).getDownloadURL().then(function(url){
+                console.log("hola"+url);
                 var img = document.getElementById(marca.name);
                 img.src = url;
-                img.onclick=setMarcaAVer(marca.id);
-
               }).catch(function(error) {
-                // Handle any errors
+                console.log(error);
               });
     }
 
@@ -48,9 +47,21 @@ export default function HomeUser(props){
         return(
                 <>
                 <Header handleLogout={handleLogout}/>
-                { marcas.map((marca)=>(
-                    <img id={marca.name} src={'images/'+marca.logo}/>
-                )) }
+                {marcaAVer?(
+                    <h1>hol {marcaAVer.name}</h1>
+                ):(
+                    <>
+                    { marcas.map((marca)=>(
+                        <>
+                        <Col xs={6} md={4}>
+                        <button onClick={()=>setMarcaAVer(marca)}><Image id={marca.name} roundedCircle /></button>
+                      </Col>
+                    </>
+                    )) }
+                    </>
+                )}
+                    
+                
                 </>
         )
     }
